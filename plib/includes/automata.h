66402b3e3b78682f6f7f7c8ac4569bece0ed6a26
REV\ReV <caginbaris@gmail.com>
2021-03-07 22:37:32 +0300

    automata revisions

--- plib/includes/automata.h
+++ plib/includes/automata.h
@@ -4,12 +4,43 @@
 
 #include <stdint.h>
 
-uint8_t on_delay(uint8_t input, uint8_t mem, uint32_t qual_sample, uint32_t *count);
-uint8_t off_delay(uint8_t input, uint8_t mem, uint32_t qual_sample, uint32_t *count);
-uint8_t on_off_delay (uint8_t input, uint8_t mem, uint32_t qual_sample, uint32_t *count);
-uint8_t on_off_delay2(uint8_t input, uint8_t mem, uint32_t onSample,uint32_t offSample, uint32_t *count);
-void pulseGen(uint32_t prd, uint32_t duty,uint8_t EN,uint8_t* out,uint8_t* latch,uint32_t* prdCount);
-uint32_t fToggle(uint32_t out,uint32_t sample, uint32_t* tCount);
+
+
+typedef struct delay_parameters {
+
+    uint8_t output;
+    uint32_t qualSample;
+    uint32_t count;
+
+}delay_parameters;
+
+typedef struct delay_parameters2 {
+
+    uint8_t output;
+    uint32_t onQualSample;
+    uint32_t offQualSample;
+    uint32_t count;
+
+}delay_parameters2;
+
+
+
+typedef struct transition_parameters {
+
+    uint8_t output;
+    uint8_t back;
+
+}transition_parameters;
+
+
+void on_delay(uint8_t input, delay_parameters* p);
+void off_delay(uint8_t input, delay_parameters* p);
+void on_off_delay(uint8_t input, delay_parameters* p);
+void on_off_delay2(uint8_t input, delay_parameters2* p);
+void low2highTransition(uint8_t input, transition_parameters* p);
+void high2lowTransition(uint8_t input, transition_parameters* p);
+void fToggle(uint8_t input, delay_parameters* p); 
+	
 
 
 void f_limiter(float* parameter,float downLimit,float upLimit);
@@ -17,12 +48,6 @@ void d_limiter(double* parameter,double downLimit,double upLimit);
 
 
 //macros
-
-#define ON_DELAY(in,out,qual,count) 				out=on_delay(in,out,qual,&count)
-#define OFF_DELAY(in,out,qual,count) 				out=off_delay(in,out,qual,&count)
-#define ON_OFF_DELAY(in,out,qual,count) 		out=on_off_delay(in,out,qual,count)
-#define ON_OFF_DELAY2(in,out,on,off,count)  out=on_off_delay2(in,out,on,off,&count)
-#define fTOGGLE(out,qual,count)             out=fToggle(out,qual,&count);
 
 #define f_LIMITER(x,down,up)								f_limiter(&x,down,up)
 #define d_LIMITER(x,down,up)								d_limiter(&x,down,up)
